d8660f0eb0a5ef36a6944225feb6664985300171
Matqui <matqui@metrocuadrado.com>
2022-02-23 15:47:56 -0500

    add javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"parcialtercio.HttpServer","l":"HttpServer"},{"p":"parcialtercio.Service","l":"ReadApiResponse"},{"p":"parcialtercio.Service","l":"ReadApiResponseImpl"}]
